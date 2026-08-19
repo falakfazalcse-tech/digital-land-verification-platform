fe61e188f7e00d7eac3f441e64110402cfa8d471
falakfazalcse-tech <falak.fazal.cse@ulab.edu.bd>
2026-08-19 15:19:18 +0600

    update_backend

--- landdeal-backend/src/config/db.js
+++ landdeal-backend/src/config/db.js
@@ -1,20 +1,16 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 24402,
+  port: Number(process.env.DB_PORT) || 3306,
   ssl: {
-    rejectUnauthorized: false 
+    rejectUnauthorized: false // Aiven MySQL SSL কানেকশনের জন্য এটি অপরিহার্য
   },
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: 10
 });
 
 module.exports = pool;
-
-
